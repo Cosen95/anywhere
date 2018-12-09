ce223a361cb0f1847a341c483db1cae085a6488a
fengshuan <1263215592@qq.com>
2018-12-09 23:38:14 +0800

    perf:抽离读取文件/文件夹（async await）

--- src/app.js
+++ src/app.js
@@ -1,34 +1,12 @@
 const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
-const fs = require('fs');
 const conf = require('./config/defaultConf');
+const route = require('./helper/route');
 
 const server = http.createServer((req,res) => {
     const filePath = path.join(conf.root,req.url);
-    fs.stat(filePath, (err, stats) => {
-        if (err) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(`${filePath} is not a directory or file`);
-            return;
-        }
-
-        if (stats.isFile()) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            // fs.readFile(filePath, (err, data) => {
-            //     res.end(data);
-            // })
-            fs.createReadStream(filePath).pipe(res);
-        } else if (stats.isDirectory()) {
-            fs.readdir(filePath, (err, files) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(files.join(','));
-            });
-        }
-    });
+    route(req, res, filePath);
 });
 
 server.listen(conf.port,conf.host, () => {
---
+++ src/helper/route.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const promisify = require('util').promisify;
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
+
+module.exports = async function (req, res, filePath) {
+    try {
+        const stats = await stat(filePath);
+        if (stats.isFile()) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            // fs.readFile(filePath, (err, data) => {
+            //     res.end(data);
+            // })
+            fs.createReadStream(filePath).pipe(res);
+        } else if (stats.isDirectory()) {
+            const files = await readdir(filePath);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(files.join(','));
+        }
+    } catch (error) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`${filePath} is not a directory or file`);
+    }
+}
