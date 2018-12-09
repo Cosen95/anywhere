0727fb2fc6431e509e5de95a3396030e43f5f0d6
fengshuan <1263215592@qq.com>
2018-12-09 23:03:09 +0800

    feat:读取文件/文件夹

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
 const http = require('http');
 const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
 const conf = require('./config/defaultConf');
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.write('<html><body><h2>anywhere</h2></body></html>');
-    res.end();
+    const filePath = path.join(conf.root,req.url);
+    fs.stat(filePath, (err, stats) => {
+        if (err) {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(`${filePath} is not a directory or file`);
+            return;
+        }
+
+        if (stats.isFile()) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            // fs.readFile(filePath, (err, data) => {
+            //     res.end(data);
+            // })
+            fs.createReadStream(filePath).pipe(res);
+        } else if (stats.isDirectory()) {
+            fs.readdir(filePath, (err, files) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(files.join(','));
+            });
+        }
+    });
 });
 
 server.listen(conf.port,conf.host, () => {
--- src/config/defaultConf.js
+++ src/config/defaultConf.js
@@ -1,4 +1,5 @@
 module.exports = {
+    root: process.cwd(),
     host: '127.0.0.1',
     port: 3000
 };
