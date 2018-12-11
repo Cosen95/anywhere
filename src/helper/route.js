a28b88c6c62109f7fccad6a2cd3f960ed5bf520e
fengshuan <1263215592@qq.com>
2018-12-11 22:30:43 +0800

    feat:Handlebars模板引擎

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConf');
+
+const tplPath = path.join(__dirname,'../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
 
 module.exports = async function (req, res, filePath) {
     try {
@@ -16,8 +23,15 @@ module.exports = async function (req, res, filePath) {
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(files.join(','));
+            res.setHeader('Content-Type', 'text/html');
+            const dir = path.relative(config.root, filePath);
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files
+            };
+            res.end(template(data));
+            // res.end(files.join(','));
         }
     } catch (error) {
         res.statusCode = 404;
